5e1edb48a1d366223b1bc2b0d72104f32f8fc570
BrownishSea <yeyopapa12@gmail.com>
2022-09-27 18:18:02 -0700

    menu-bien hecho
    borrador

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('1234')
+const hamMenu = document.querySelector('.hamburger-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
