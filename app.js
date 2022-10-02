cd271e2c5a6f25a336d5dfac3b90c5b6b052f0ac
BrownishSea <yeyopapa12@gmail.com>
2022-10-02 16:10:12 -0700

    muchas cosas

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const acordiones = document.querySelectorAll('.acordion')
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+});
+acordiones.forEach(acordion => {
+    acordion.addEventListener('click', e => {
+        let botonAcordion = acordion.querySelector('button');
+        acordion.classList.toggle('active');
+        if (botonAcordion.textContent === '+') {
+            botonAcordion.textContent = '-';
+        } else {
+            botonAcordion.textContent = '+';
+        }
+    })
 })
