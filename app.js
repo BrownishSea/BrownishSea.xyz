a46fa96d776b324b602f8943b4b6c89cfc4a34ee
BrownishSea <87672539+BrownishSea@users.noreply.github.com>
2022-12-09 09:23:30 -0700

    sdasd

--- app.js
+++ app.js
@@ -7,13 +7,9 @@ hamMenu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active');
 });
 
-/*Este codigo hace que cada acordion se toglee para 
-mostrar el body cuando el header es clickeado esto se hace mayormente con 
-css esto es nomas para que funcione bien*/
 acordiones.forEach(acordion => {
     acordion.addEventListener('click', e => {
         acordion.classList.toggle('active');
-        //Esta parte es solo para cambiar el mas por un menos
         let botonAcordion = acordion.querySelector('button');
         if (botonAcordion.textContent === '+') {
             botonAcordion.textContent = '-';
